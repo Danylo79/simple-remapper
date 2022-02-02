e633710a4acc25f8230f21cf2a16865576ddc4d2
Danylo79 <dan.komisarenko@gmail.com>
2022-02-02 09:43:49 -0500

    More mapping processors

--- src/main/java/dev/dankom/sremapper/processor/MappingProcessor.java
+++ src/main/java/dev/dankom/sremapper/processor/MappingProcessor.java
@@ -5,65 +5,124 @@ import dev.dankom.sremapper.mapping.Mapping;
 import dev.dankom.sremapper.reader.base.MappingReader;
 import dev.dankom.sremapper.writer.MappingWriter;
 
-import java.io.File;
+import java.io.*;
 import java.util.List;
 
-public interface MappingProcessor<T extends Mapping> {
-    void writeClass(StringBuffer buffer, ClassMapping<T> mapping);
-    void writeField(StringBuffer buffer, T mapping);
-    void writeMethod(StringBuffer buffer, T mapping);
+public interface MappingProcessor<T extends Mapping, E extends ClassMapping<T>> {
+    void writeClass(BufferedWriter buffer, E mapping);
+    void writeField(BufferedWriter buffer, T mapping);
+    void writeMethod(BufferedWriter buffer, T mapping);
 
+    boolean isComment(String line);
     boolean isClass(String line);
     boolean isField(String line);
     boolean isMethod(String line);
 
-    T readClass(String line);
-    T readField(String line);
-    T readMethod(String line);
+    void readClass(String line);
+    void readField(String line);
+    void readMethod(String line);
 
-    static <T extends Mapping> MappingProcessorReader makeReader(File in, MappingProcessor<T> processor) {
+    static <T extends Mapping, E extends ClassMapping<T>> MappingProcessorReader makeReader(File in, MappingProcessor<T, E> processor) {
         return new MappingProcessorReader(in, processor);
     }
 
-    static <T extends Mapping> MappingProcessorWriter makeWriter(List<T> mappings, File out, MappingProcessor<T> processor) {
+    static <T extends Mapping, E extends ClassMapping<T>> MappingProcessorWriter makeWriter(List<T> mappings, File out, MappingProcessor<T, E> processor) {
         return new MappingProcessorWriter(mappings, out, processor);
     }
 
-    class MappingProcessorReader<T extends Mapping> extends MappingReader {
-        private MappingProcessor<T> processor;
+    class MappingProcessorReader<T extends Mapping, E extends ClassMapping<T>> extends MappingReader {
+        private MappingProcessor<T, E> processor;
 
-        public MappingProcessorReader(File in, MappingProcessor<T> processor) {
+        public MappingProcessorReader(File in, MappingProcessor<T, E> processor) {
             super(in);
             this.processor = processor;
         }
 
         @Override
         public void read() {
+            findClasses();
 
+            try {
+                FileReader reader = new FileReader(in);
+                BufferedReader buf = new BufferedReader(reader);
+                boolean loop = true;
+                while (loop) {
+                    String s = buf.readLine();
+                    if (s != null && !s.isEmpty()) {
+                        if (processor.isComment(s)) continue;
+
+                        if (!processor.isClass(s)) {
+                            if (processor.isMethod(s)) {
+                                processor.readMethod(s);
+                            } else if (processor.isField(s)) {
+                                processor.readField(s);
+                            }
+                        } else {
+                            processor.readClass(s);
+                        }
+                    } else loop = false;
+                }
+
+                buf.close();
+                reader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void findClasses() {
+            try {
+                FileReader reader = new FileReader(in);
+                BufferedReader buf = new BufferedReader(reader);
+                boolean loop = true;
+                while (loop) {
+                    String s = buf.readLine();
+
+                    if (s != null && !s.isEmpty()) {
+                        if (processor.isComment(s)) continue;
+
+                        if (processor.isClass(s)) {
+                            processor.readClass(s);
+                        }
+                    } else loop = false;
+                }
+
+                buf.close();
+                reader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
     class MappingProcessorWriter<T extends Mapping, E extends ClassMapping<T>> extends MappingWriter<T, E> {
-        private MappingProcessor<T> processor;
+        private MappingProcessor<T, E> processor;
 
-        public MappingProcessorWriter(List<E> mappings, File out, MappingProcessor<T> processor) {
+        public MappingProcessorWriter(List<E> mappings, File out, MappingProcessor<T, E> processor) {
             super(mappings, out);
             this.processor = processor;
         }
 
         @Override
         public void write() {
-            StringBuffer buffer = new StringBuffer();
-            for (ClassMapping<T> mapping : mappings) {
-                processor.writeClass(buffer, mapping);
+            try {
+                FileWriter fw = new FileWriter(out);
+                BufferedWriter buffer = new BufferedWriter(fw);
+                for (E mapping : mappings) {
+                    processor.writeClass(buffer, mapping);
 
-                for (T field : mapping.getFields()) {
-                    processor.writeField(buffer, field);
-                }
+                    for (T field : mapping.getFields()) {
+                        processor.writeField(buffer, field);
+                    }
 
-                for (T method : mapping.getMethods()) {
-                    processor.writeMethod(buffer, method);
+                    for (T method : mapping.getMethods()) {
+                        processor.writeMethod(buffer, method);
+                    }
                 }
+                buffer.close();
+                fw.close();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
